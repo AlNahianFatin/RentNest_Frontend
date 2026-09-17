f987db797fa17e5016e5341187196523e105e2ab
AlNahianFatin <fatinnahian@gmail.com>
2026-09-18 02:21:05 +0600

    Added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,28 @@ import "./globals.css";
 import { Raleway, Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Toaster } from "sonner";
+import type { Metadata } from "next";
 
-const ralewayHeading = Raleway({subsets:['latin'],variable:'--font-heading'});
+const ralewayHeading = Raleway({ subsets: ['latin'], variable: '--font-heading' });
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
+export const metadata: Metadata = {
+  title: "Rent Nest",
+  description: "Find your perfect rental property with RentNest.",
+  icons: {
+    icon: "/RentNestLogo.svg",
+  },
+};
 
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en" className={cn("h-full", "antialiased", "font-sans", inter.variable, ralewayHeading.variable)}>
+      {/* <head>
+        <link rel="icon" type="image/svg" href="/RentNestLogo.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Rent Nest</title>
+      </head> */}
       <body className="min-h-full flex flex-col">
         {/* Navbar */}
         <Toaster position="top-right" richColors />
